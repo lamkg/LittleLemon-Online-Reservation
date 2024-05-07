eb72b7b6bac20a885283f91ac2564f54096f9bb3
lamkg <lamkevin93@gmail.com>
2024-05-07 16:25:44 -0700

    semantic edit

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -36,7 +36,7 @@ const handleOrder = (id) => {
   return (
     <div className="menu-container">
       <div className="menu-header">
-        <h2>This weeks specials!</h2>
+        <h2>This Week's Specials!</h2>
         <button>Online Menu</button>
       </div>
       <div className="cards">
