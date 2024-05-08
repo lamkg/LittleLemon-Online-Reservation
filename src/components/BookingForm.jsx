a4d13a3eb3c3794936b417b02e5189b840ae6f5b
lamkg <lamkevin93@gmail.com>
2024-05-08 16:25:54 -0700

    passed props so that confirmedbooking would look clean

--- src/components/BookingForm.jsx
+++ src/components/BookingForm.jsx
@@ -1,49 +1,67 @@
-import React from "react";
-import { useState } from "react";
-
-
-
+import React, { useState } from "react";
 
 const BookingForm = (props) => {
-
   const [occasion, setOccasion] = useState("");
   const [guests, setGuests] = useState("");
   const [date, setDate] = useState("");
-  const [times, setTimes] = useState("")
+  const [times, setTimes] = useState("");
   const [email, setEmail] = useState("");
+  const [formData, setFormData] = useState({});
 
-
-  const handleSumbit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    props.submitForm(e);
+    const data = {
+      occasion,
+      guests,
+      date,
+      times,
+      email,
+    };
+    setFormData(data);
+    props.submitForm(data);
   };
-
-  const handleChange = (e) => {
-    setDate(e);
-    props.dispatch(e);
-  }
-
-
 
   return (
     <header>
       <section>
-        <form onSubmit={handleSumbit}>
+        <form onSubmit={handleSubmit}>
           <fieldset className="formField">
             <div>
               <label htmlFor="book-date">Choose Date:</label>
-              <input id="book-date" value={date} onChange={(e) => handleChange(e.target.value)} type="date" required />
+              <input
+                id="book-date"
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
+                type="date"
+                required
+              />
             </div>
             <div>
               <label htmlFor="book-time">Choose Time:</label>
-              <select id="book-time" value={times} onChange={(e) => setTimes(e.target.value)} required>
+              <select
+                id="book-time"
+                value={times}
+                onChange={(e) => setTimes(e.target.value)}
+                required
+              >
                 <option value="">Select a Time</option>
-                {props.availableTimes.availableTimes.map(availableTimes => { return <option key={availableTimes}>{availableTimes}</option> })}
+                {props.availableTimes.map((availableTime) => (
+                  <option key={availableTime}>{availableTime}</option>
+                ))}
               </select>
             </div>
             <div>
               <label htmlFor="book-guests">Number of Guests:</label>
-              <input id="book-guests" min="1" value={guests} onChange={(e) => { setGuests(e.target.value) }} type={"number"} placeholder={0} max={10} required></input>
+              <input
+                id="book-guests"
+                min="1"
+                value={guests}
+                onChange={(e) => setGuests(e.target.value)}
+                type={"number"}
+                placeholder={0}
+                max={10}
+                required
+              />
             </div>
             <div>
               <label htmlFor="book-email">Email Address:</label>
@@ -57,7 +75,12 @@ const BookingForm = (props) => {
             </div>
             <div>
               <label htmlFor="book-occasion">Occasion:</label>
-              <select id="book-occasion" key={occasion} value={occasion} onChange={(e) => setOccasion(e.target.value)} required>
+              <select
+                id="book-occasion"
+                value={occasion}
+                onChange={(e) => setOccasion(e.target.value)}
+                required
+              >
                 <option value="">Select an Option</option>
                 <option>Birthday</option>
                 <option>Anniversary</option>
@@ -65,7 +88,11 @@ const BookingForm = (props) => {
               </select>
             </div>
             <div className="btnReceive">
-              <input aria-label="On Click" type={"submit"} value={"Make Your Reservation"}></input>
+              <input
+                aria-label="On Click"
+                type={"submit"}
+                value={"Make Your Reservation"}
+              />
             </div>
           </fieldset>
         </form>
--- src/components/ConfirmedBooking.jsx
+++ src/components/ConfirmedBooking.jsx
@@ -1,17 +1,19 @@
 import React from "react";
 
-const ConfirmedBooking = () => {
-    return (
-        <div className="confirm">
-          <div>
-            <h1>
-              Booking has been <span>confirmed!</span>
-            </h1>
-            <h3>Confirmation email has been sent!</h3>
+const ConfirmedBooking = ({ formData }) => {
+  const { email, guests, times } = formData;
 
-          </div>
-        </div>
-    )
+  return (
+    <div className="confirm">
+      <div>
+        <h1>
+          Booking has been <span>confirmed!</span>
+        </h1>
+        <h3>Confirmation email has been sent to {email}</h3>
+        <p>There are {guests} guests for {times}</p>
+      </div>
+    </div>
+  );
 }
 
 export default ConfirmedBooking;
